d6353bcd8f25e08820f7ad226bce3a76d27da68a
unknown <genecort@espol.edu.ec>
2022-01-18 12:59:41 -0500

    CustomerTest

--- RentalSys/RentalSystem/src/test/java/com/espol/rentalsystemtest/CustomerTest.java
+++ RentalSys/RentalSystem/src/test/java/com/espol/rentalsystemtest/CustomerTest.java
@@ -21,32 +21,30 @@ public class CustomerTest {
     public CustomerTest() {
     }
 
-    @org.junit.BeforeClass
-    public static void setUpClass() throws Exception {
-    }
-
-    @org.junit.AfterClass
-    public static void tearDownClass() throws Exception {
-    }
-
-    @org.junit.Before
-    public void setUp() throws Exception {
-    }
-
-    @org.junit.After
-    public void tearDown() throws Exception {
-    }
+    Customer instance;
+    Movie movie;
+    MovieRental movRental;
+    Ps3Game psGame ;
+    VideoGameRental gameRental ;
     
     @BeforeClass
     public static void setUpClass() {
+       
     }
     
     @AfterClass
     public static void tearDownClass() {
     }
-    
     @Before
     public void setUp() {
+         instance = new Customer("Morty Smith");
+        movie = new Movie("Alien Invasion Tomato Monster Mexican "
+                + "Armada Brothers Who Are Just Regular Brothers "
+                + "Running In A Van From An Asteroid And All Sorts"
+                + " Of Things The Movie",Movie.REGULAR);
+        movRental = new MovieRental(movie, 7);
+        psGame = new Ps3Game("Resident Evil Village");
+        gameRental = new VideoGameRental(psGame, 7, false);
     }
     
     @After
@@ -56,27 +54,26 @@ public class CustomerTest {
     /**
      * Test of addMovieRental method, of class Customer.
      */
+    
+
     @org.junit.Test
     public void testAddMovieRental() {
         System.out.println("addMovieRental");
-        MovieRental arg = null;
-        Customer instance = null;
-        instance.addMovieRental(arg);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        instance.addMovieRental(movRental);
+        
+        String verify = instance.statement();
+        assertTrue("Pelicula agg en lista",verify.contains(movie._title));
     }
-
     /**
      * Test of addVideoGameRental method, of class Customer.
      */
     @org.junit.Test
     public void testAddVideoGameRental() {
         System.out.println("addVideoGameRental");
-        VideoGameRental arg = null;
-        Customer instance = null;
-        instance.addVideoGameRental(arg);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        instance.addVideoGameRental(gameRental);
+        String verify = instance.statement();
+        Ps3Game psg = (Ps3Game)gameRental.getVideoGame();
+        assertTrue("Videojuego agg en lista",verify.contains(psg.get_gameName()));
     }
 
     /**
@@ -84,13 +81,30 @@ public class CustomerTest {
      */
     @org.junit.Test
     public void testStatement() {
+        float total=0;
+        int freq = 0;
+        
+        instance.addVideoGameRental(gameRental);
+        instance.addMovieRental(movRental);
+
         System.out.println("statement");
-        Customer instance = null;
-        String expResult = "";
+        
+        String expresult = "Rental Record for " + instance._name + "\n";
+        expresult += "\t" + movRental.getMovie()._title+ "\t"
+                    + String.valueOf(9.5) + "\n";
+        total += 9.5;
+        freq++;
+        expresult += "\t" + gameRental.getVideoGame() + "\t"
+            		+ String.valueOf(gameRental.getCharge()) + "\n";
+            total += gameRental.getCharge();
+        expresult += "Amount owed is " + String.valueOf(total) + "\n";
+        expresult += "You earned " + String.valueOf(freq+gameRental.getFrequentRenterPoints())
+                + " frequent renter points";
+            
+        
         String result = instance.statement();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertEquals(expresult, result);
+        
     }
     
 }
