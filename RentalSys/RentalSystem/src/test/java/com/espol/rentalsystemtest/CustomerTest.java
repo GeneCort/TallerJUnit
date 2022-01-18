0c2f3113f853dd14d0ab821108a69599fdd1c0f3
unknown <genecort@espol.edu.ec>
2022-01-18 09:55:19 -0500

    Proyecto base

---
+++ RentalSys/RentalSystem/src/test/java/com/espol/rentalsystemtest/CustomerTest.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.espol.rentalsystemtest;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author gene
+ */
+public class CustomerTest {
+    
+    public CustomerTest() {
+    }
+
+    @org.junit.BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @org.junit.AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of addMovieRental method, of class Customer.
+     */
+    @org.junit.Test
+    public void testAddMovieRental() {
+        System.out.println("addMovieRental");
+        MovieRental arg = null;
+        Customer instance = null;
+        instance.addMovieRental(arg);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of addVideoGameRental method, of class Customer.
+     */
+    @org.junit.Test
+    public void testAddVideoGameRental() {
+        System.out.println("addVideoGameRental");
+        VideoGameRental arg = null;
+        Customer instance = null;
+        instance.addVideoGameRental(arg);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of statement method, of class Customer.
+     */
+    @org.junit.Test
+    public void testStatement() {
+        System.out.println("statement");
+        Customer instance = null;
+        String expResult = "";
+        String result = instance.statement();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
